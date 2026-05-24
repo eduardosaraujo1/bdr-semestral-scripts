ffd253abc3ea5e6e303475aa279a01eef92da2ed
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-05-24 14:13:18 -0300

    atualizado create.sql

--- fisico/Create.sql
+++ fisico/Create.sql
@@ -1,20 +1,13 @@
 CREATE SEQUENCE SQ_AVALIACAO;
-/
 CREATE SEQUENCE SQ_OFERTA;
-/
 CREATE SEQUENCE SQ_MATRICULA;
-/
 CREATE SEQUENCE SQ_TURMA;
-/
 CREATE SEQUENCE SQ_DISCIPLINA;
-/
 CREATE SEQUENCE SQ_CURSO;
-/
 CREATE SEQUENCE SQ_PROFESSOR;
-/
 CREATE SEQUENCE SQ_ALUNO;
-/
-CREATE TABLE aluno (
+
+CREATE TABLE ALUNO (
   id_aluno INTEGER PRIMARY KEY,
   nm_aluno VARCHAR(255) NOT NULL,
   cpf_aluno CHAR(11) NOT NULL,
@@ -23,8 +16,8 @@ CREATE TABLE aluno (
   nm_endereco_aluno VARCHAR(255) NOT NULL,
   cd_situacao_aluno NUMBER(4) NOT NULL
 );
-/
-CREATE TABLE professor (
+
+CREATE TABLE PROFESSOR (
   id_professor INTEGER PRIMARY KEY,
   nm_professor VARCHAR(255) NOT NULL,
   cpf_professor CHAR(11) NOT NULL,
@@ -32,8 +25,8 @@ CREATE TABLE professor (
   nm_especialidade_professor VARCHAR(255) NOT NULL,
   dt_admissao_professor DATE NOT NULL
 );
-/
-CREATE TABLE curso (
+
+CREATE TABLE CURSO (
   id_curso INTEGER PRIMARY KEY,
   sg_curso CHAR(10) NOT NULL,
   nm_curso VARCHAR(255) NOT NULL,
@@ -45,8 +38,8 @@ CREATE TABLE curso (
   dt_ativacao_curso DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
   dt_desativacao_curso DATE
 );
-/
-CREATE TABLE disciplina (
+
+CREATE TABLE DISCIPLINA (
   id_disciplina INTEGER PRIMARY KEY,
   sg_disciplina CHAR(16) NOT NULL,
   nm_disciplina VARCHAR(255) NOT NULL,
@@ -55,70 +48,50 @@ CREATE TABLE disciplina (
   dt_ativacao_disciplina DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
   dt_desativacao_disciplina DATE
 );
-/
-CREATE TABLE disciplina_curso (
-  id_disciplina INTEGER,
-  id_curso INTEGER,
-  id_ciclo_disciplina_curso NUMBER(2),
-  PRIMARY KEY (id_disciplina, id_curso),
-  FOREIGN KEY (id_disciplina)
-    REFERENCES disciplina (id_disciplina),
-  FOREIGN KEY (id_curso)
-    REFERENCES curso (id_curso)
-);
-/
-CREATE TABLE turma (
+
+CREATE TABLE TURMA (
   id_turma INTEGER PRIMARY KEY,
   dt_inicio_turma DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
   dt_fim_turma DATE,
   id_ciclo_turma NUMBER(2) NOT NULL,
   id_curso INTEGER NOT NULL,
-  FOREIGN KEY (id_curso)
-    REFERENCES curso (id_curso)
+  FOREIGN KEY (id_curso) REFERENCES CURSO (id_curso) DEFERRABLE INITIALLY IMMEDIATE
 );
-/
-CREATE TABLE disciplina_turma (
+
+CREATE TABLE DISCIPLINA_TURMA (
   id_turma INTEGER,
   id_disciplina INTEGER,
   qt_vagas_disciplina_turma NUMBER(3) NOT NULL,
   id_professor INTEGER NOT NULL,
   PRIMARY KEY (id_turma, id_disciplina),
-  FOREIGN KEY (id_turma)
-    REFERENCES turma (id_turma),
-  FOREIGN KEY (id_disciplina)
-    REFERENCES disciplina (id_disciplina),
-  FOREIGN KEY (id_professor)
-    REFERENCES professor (id_professor)
+  FOREIGN KEY (id_turma) REFERENCES TURMA (id_turma) DEFERRABLE INITIALLY IMMEDIATE,
+  FOREIGN KEY (id_disciplina) REFERENCES DISCIPLINA (id_disciplina) DEFERRABLE INITIALLY IMMEDIATE,
+  FOREIGN KEY (id_professor) REFERENCES PROFESSOR (id_professor) DEFERRABLE INITIALLY IMMEDIATE
 );
-/
-CREATE TABLE matricula (
-  id_matricula INTEGER PRIMARY KEY,
-  id_turma INTEGER NOT NULL,
-  id_disciplina INTEGER NOT NULL,
-  id_aluno INTEGER NOT NULL,
-  CONSTRAINT uk_matricula
-    UNIQUE (id_turma, id_disciplina, id_aluno),
-  FOREIGN KEY (id_aluno)
-    REFERENCES aluno (id_aluno),
-  FOREIGN KEY (id_turma, id_disciplina)
-    REFERENCES disciplina_turma (id_turma, id_disciplina)
-);
-/
-CREATE TABLE oferta (
+
+CREATE TABLE OFERTA (
   id_oferta INTEGER PRIMARY KEY,
   cd_sala_oferta VARCHAR(20) NOT NULL,
   dd_semana_oferta CHAR(3) NOT NULL,
   hr_inicio_oferta NUMBER(4) NOT NULL,
   hr_fim_oferta NUMBER(4) NOT NULL,
+  id_disciplina INTEGER NOT NULL,
+  id_turma INTEGER NOT NULL,
+  CONSTRAINT chk_dia_semana CHECK (dd_semana_oferta IN ('SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB', 'DOM')),
+  FOREIGN KEY (id_turma, id_disciplina) REFERENCES DISCIPLINA_TURMA (id_turma, id_disciplina) DEFERRABLE INITIALLY IMMEDIATE
+);
+
+CREATE TABLE MATRICULA (
+  id_matricula INTEGER PRIMARY KEY,
   id_turma INTEGER NOT NULL,
   id_disciplina INTEGER NOT NULL,
-  CONSTRAINT chk_dia_semana
-    CHECK (dd_semana_oferta IN ('SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB', 'DOM')),
-  FOREIGN KEY (id_turma, id_disciplina)
-    REFERENCES disciplina_turma (id_turma, id_disciplina)
+  id_aluno INTEGER NOT NULL,
+  CONSTRAINT unique_matricula UNIQUE (id_turma, id_disciplina, id_aluno),
+  FOREIGN KEY (id_aluno) REFERENCES ALUNO (id_aluno) DEFERRABLE INITIALLY IMMEDIATE,
+  FOREIGN KEY (id_turma, id_disciplina) REFERENCES DISCIPLINA_TURMA (id_turma, id_disciplina) DEFERRABLE INITIALLY IMMEDIATE
 );
-/
-CREATE TABLE avaliacao (
+
+CREATE TABLE AVALIACAO (
   id_avaliacao INTEGER PRIMARY KEY,
   ds_avaliacao VARCHAR(255),
   qt_peso_avaliacao NUMBER(3,1) NOT NULL,
@@ -127,38 +100,31 @@ CREATE TABLE avaliacao (
   cd_tipo_avaliacao NUMBER(4) NOT NULL,
   id_turma INTEGER NOT NULL,
   id_disciplina INTEGER NOT NULL,
-  FOREIGN KEY (id_turma, id_disciplina)
-    REFERENCES disciplina_turma (id_turma, id_disciplina)
+  FOREIGN KEY (id_turma, id_disciplina) REFERENCES DISCIPLINA_TURMA (id_turma, id_disciplina) DEFERRABLE INITIALLY IMMEDIATE
 );
-/
-CREATE TABLE registro_aula (
-  dt_registro_aula DATE,
-  id_oferta INTEGER,
+
+CREATE TABLE REGISTRO_AULA (
+  dt_registro_aula DATE NOT NULL,
+  id_oferta INTEGER NOT NULL,
   PRIMARY KEY (dt_registro_aula, id_oferta),
-  CONSTRAINT chk_dt_registro_aula
-    CHECK (dt_registro_aula = TRUNC(dt_registro_aula)),
-  FOREIGN KEY (id_oferta)
-    REFERENCES oferta (id_oferta)
+  CHECK (dt_registro_aula = TRUNC(dt_registro_aula)),
+  FOREIGN KEY (id_oferta) REFERENCES OFERTA (id_oferta) DEFERRABLE INITIALLY IMMEDIATE
 );
-/
-CREATE TABLE avaliacao_resultado (
-  id_avaliacao INTEGER,
-  id_matricula INTEGER,
+
+CREATE TABLE AVALIACAO_RESULTADO (
+  id_avaliacao INTEGER NOT NULL,
+  id_matricula INTEGER NOT NULL,
   qt_nota_avaliacao_aluno NUMBER(4,2) NOT NULL,
   PRIMARY KEY (id_avaliacao, id_matricula),
-  FOREIGN KEY (id_avaliacao)
-    REFERENCES avaliacao (id_avaliacao),
-  FOREIGN KEY (id_matricula)
-    REFERENCES matricula (id_matricula)
+  FOREIGN KEY (id_avaliacao) REFERENCES AVALIACAO (id_avaliacao) DEFERRABLE INITIALLY IMMEDIATE,
+  FOREIGN KEY (id_matricula) REFERENCES MATRICULA (id_matricula) DEFERRABLE INITIALLY IMMEDIATE
 );
-/
-CREATE TABLE aluno_registro_aula (
-  dt_registro_aula DATE,
-  id_oferta INTEGER,
+
+CREATE TABLE ALUNO_REGISTRO_AULA (
+  dt_registro_aula DATE NOT NULL,
+  id_oferta INTEGER NOT NULL,
   id_matricula INTEGER,
   PRIMARY KEY (dt_registro_aula, id_oferta, id_matricula),
-  FOREIGN KEY (id_matricula)
-    REFERENCES matricula (id_matricula),
-  FOREIGN KEY (dt_registro_aula, id_oferta)
-    REFERENCES registro_aula (dt_registro_aula, id_oferta)
+  FOREIGN KEY (id_matricula) REFERENCES MATRICULA (id_matricula) DEFERRABLE INITIALLY IMMEDIATE,
+  FOREIGN KEY (dt_registro_aula, id_oferta) REFERENCES REGISTRO_AULA (dt_registro_aula, id_oferta) DEFERRABLE INITIALLY IMMEDIATE
 );
